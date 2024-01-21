1e61f189cb23a8e348d6153c455018813d125c02
Nishant Kumar <37207638+knishant0808@users.noreply.github.com>
2024-01-22 00:07:27 +0530

    Update indexRoutes.js

--- server/routes/api/indexRoutes.js
+++ server/routes/api/indexRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Import other route files
-// const userRoutes = require('./userRoutes');
-// const postRoutes = require('./postRoutes');
-
 // Use doctorRoutes
 const doctorRoutes = require('./doctorRoutes');
 router.use('/doctor', doctorRoutes);
